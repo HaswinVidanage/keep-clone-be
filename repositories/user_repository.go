a102dac3aa08eb3acc350df4f7ea4b6bd75631c5
Haswin <hashdevan@gmail.com>
2020-11-27 18:51:35 +0800

    feature/unit-tests: * Add unit tests - user repository

--- repositories/user_repository.go
+++ repositories/user_repository.go
@@ -3,6 +3,7 @@ package repositories
 import (
 	"context"
 	"database/sql"
+	sq "github.com/elgris/sqrl"
 	"github.com/google/wire"
 	"github.com/sirupsen/logrus"
 	"hackernews-api/entities"
@@ -13,6 +14,14 @@ import (
 type IUserRepository interface {
 	InsertUser(context.Context, entities.CreateUser) (int, error)
 	GetUserIdByEmail(context.Context, string) (int, error)
+	// UpdateUserByFields
+	UpdateUser(ctx context.Context, u entities.User) (int, error)
+	// DeleteUser
+	// DeleteUserByID
+	// FindAllUser
+	FindUserByID(ctx context.Context, id int) (entities.User, error)
+	// UsersByEmail
+	UpdateUserBaseQuery(ctx context.Context, u entities.User) *sq.UpdateBuilder
 }
 
 type UserRepository struct {
@@ -63,4 +72,48 @@ func (ur *UserRepository) GetUserIdByEmail(ctx context.Context, email string) (i
 	}
 
 	return Id, nil
+}
+
+func (ur *UserRepository) UpdateUser(ctx context.Context, u entities.User) (int, error) {
+	qb := ur.UpdateUserBaseQuery(ctx, u)
+	// run query
+	result, err := qb.RunWith(ur.DbProvider.Db).Exec()
+	if err != nil {
+		logrus.WithError(err).Warn(err)
+		return 0, err
+	}
+
+	lastId, err := result.LastInsertId()
+	if err != nil {
+		logrus.WithError(err).Warn(err)
+		return 0, err
+	}
+
+	return int(lastId), nil
+}
+
+func (ur *UserRepository) FindUserByID(ctx context.Context, id int) (entities.User, error) {
+	statement, err := ur.DbProvider.Db.Prepare("select * from user WHERE id = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	row := statement.QueryRow(id)
+	var user entities.User
+	err = row.Scan(&user.ID, &user.Name, &user.Email)
+	if err != nil {
+		if err != sql.ErrNoRows {
+			log.Print(err)
+		}
+		return entities.User{}, err
+	}
+	return user, nil
+}
+
+// Get queries
+func (ur *UserRepository) UpdateUserBaseQuery(ctx context.Context, u entities.User) *sq.UpdateBuilder {
+	qb := sq.Update("`user`").SetMap(map[string]interface{}{
+		"`name`":  u.Name,
+		"`email`": u.Email,
+	}).Where(sq.Eq{"`id`": u.ID})
+	return qb
 }
--- repositories/user_repository_test.go
+++ repositories/user_repository_test.go
@@ -14,7 +14,7 @@ type UserReposirotyTestSuite struct {
 	suite.Suite
 }
 
-func NewUserReposirotyTestSuite() *UserReposirotyTestSuite {
+func NewUserRepositoryTestSuite() *UserReposirotyTestSuite {
 	app, err := wire.GetTestApp()
 	if err != nil {
 		panic(err)
@@ -22,8 +22,8 @@ func NewUserReposirotyTestSuite() *UserReposirotyTestSuite {
 	return &UserReposirotyTestSuite{TestApp: app}
 }
 
-func TestVisitService(t *testing.T) {
-	var testSuite *UserReposirotyTestSuite = NewUserReposirotyTestSuite()
+func TestUserRepository(t *testing.T) {
+	var testSuite *UserReposirotyTestSuite = NewUserRepositoryTestSuite()
 	suite.Run(t, testSuite)
 }
 
@@ -62,6 +62,52 @@ func (s *UserReposirotyTestSuite) Test_GetUserIdByEmail() {
 	lastId, err := s.Resolver.IUserRepository.GetUserIdByEmail(s.Ctx, email)
 	s.Nil(err)
 	s.Equal(1, lastId)
+
+	// we make sure that all expectations were met
+	err = s.Mock.ExpectationsWereMet()
+	s.Nil(err)
+}
+
+func (s *UserReposirotyTestSuite) Test_UpdateUser() {
+	user := entities.User{
+		ID:    "1",
+		Name:  "john",
+		Email: "haswin@gmail.com",
+	}
+
+	qb := s.Resolver.IUserRepository.UpdateUserBaseQuery(s.Ctx, user)
+	query, _, err := qb.ToSql()
+	s.NoError(err)
+	s.Mock.ExpectExec(query).WithArgs(user.Email, user.Name, user.ID).WillReturnResult(sqlmock.NewResult(1, 1))
+
+	// now we execute our method
+	lastId, err := s.Resolver.IUserRepository.UpdateUser(s.Ctx, user)
+	s.Nil(err)
+	s.Equal(1, lastId)
+
+	// we make sure that all expectations were met
+	err = s.Mock.ExpectationsWereMet()
+	s.Nil(err)
+}
+
+func (s *UserReposirotyTestSuite) Test_FindUserByID() {
+	user := entities.User{
+		ID:    "1",
+		Name:  "john",
+		Email: "haswin@gmail.com",
+	}
+
+	query := "select * from user WHERE id = ?"
+	rows := sqlmock.NewRows([]string{"id", "name", "email"}).
+		AddRow(1, user.Name, user.Email)
+
+	s.Mock.ExpectPrepare(query)
+	s.Mock.ExpectQuery(query).WithArgs(1).WillReturnRows(rows)
+
+	// now we execute our method
+	user, err := s.Resolver.IUserRepository.FindUserByID(s.Ctx, 1)
+	s.Nil(err)
+	s.Equal("1", user.ID)
 
 	// we make sure that all expectations were met
 	err = s.Mock.ExpectationsWereMet()
