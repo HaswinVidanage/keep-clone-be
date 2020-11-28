8bcb786f83ee863b192f42afec8593e04b987a91
Haswin <hashdevan@gmail.com>
2020-11-29 01:28:29 +0800

    feature/unit-tests: * Add run.sh

---
+++ run.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+migrate() {
+    if [[ $1 != '--down' ]]; then
+        migrate -database "mysql://sa:qweqwe@tcp(localhost:3305)/hackernews_db" -path internal/pkg/db/migrations/mysql down
+    else
+        migrate -database "mysql://sa:qweqwe@tcp(localhost:3305)/hackernews_db" -path internal/pkg/db/migrations/mysql up
+    fi
+}
+
+genAll() {
+    go generate ./...
+}
+
+test() {
+    go test ./...
+}
+
+if [[ $1 = 'migrate' ]]; then
+    migrate $2
+elif [[ $1 = 'gen-all' ]]; then
+    genAll
+elif [[ $1 = 'test' ]]; then
+    test
+else
+    echo "Usage: ./run.sh (migrate|test|gen-all)"
+    exit 2
+fi
