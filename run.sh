07780e79d1af7a21b9f7485e2324c143b8db80a3
Haswin <hashdevan@gmail.com>
2020-12-11 12:32:29 +0800

    feature/setup-ci-cd: * Refactor docker setup.

--- run.sh
+++ run.sh
@@ -6,7 +6,6 @@ migrate() {
     if [[ -z "${MYSQL_USER}" ]]; then
         source docker.env
     fi
-
     connectionString="mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@localhost:3305/${MYSQL_DATABASE}"
     if [[ $1 != '--down' ]]; then
         migrate -database $connectionString -path internal/pkg/db/migrations/mysql down
@@ -33,25 +32,16 @@ test() {
     echo "Test exited with exit code $?"
 }
 
-dockerBuild() {
- docker-compose build -t keep-app .
-#   docker build  -f ./docker/prod/Go/Dockerfile -t keep-app .
-}
-
-dockerStop(){
-    docker container stop $(docker container ls -aq)
-    docker container rm $(docker container ls -aq)
-}
 
 if [[ $1 = 'migrate' ]]; then
     migrate $2
 elif [[ $1 = 'gen-all' ]]; then
     genAll
-elif [[ $1 = 'docker-build' ]]; then
-    dockerBuild
+elif [[ $1 = 'deploy' ]]; then
+    deploy
 elif [[ $1 = 'test' ]]; then
     test
 else
-    echo "Usage: ./run.sh (migrate|test|gen-all)"
+    echo "Usage: ./run.sh (migrate|test|gen-all|deploy)"
     exit 2
 fi
